0e4502d4a3351a0d8d6ffcd15f42eb0c207ec4c8
sowmik-sec <sowmik.sec@gmail.com>
2024-10-23 02:22:15 +0600

    [DONE] Create User Route , Controller _ test utility function

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express, { Application, Request, Response } from 'express'
+import usersRouter from './app/modules/users/user.route'
 const app: Application = express()
 import cors from 'cors'
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// application routes
+
+app.use('/api/v1/users', usersRouter)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
--- src/app/modules/users/users.utils.ts
+++ src/app/modules/users/users.utils.ts
@@ -11,7 +11,6 @@ export const findLastUserId = async () => {
 
 export const generateUserId = async () => {
   const currentId = (await findLastUserId()) || (0).toString().padStart(5, '0')
-  return currentId
-  //   lastUserId++
-  //   return String(lastUserId).padStart(5, '0')
+  const incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0')
+  return incrementedId
 }
