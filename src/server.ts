6aa63798d28688d568e5cc7ae10a8e16d7d76275
sowmik-sec <sowmik.sec@gmail.com>
2024-10-23 11:20:49 +0600

    setup logger, customize it and use it

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,20 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
+import { logger, errorLogger } from './shsred/logger'
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log(`Successfully connected to db`)
+    logger.info(`Successfully connected to db`)
     app.listen(config.port, () => {
-      console.log(
+      logger.info(
         `University management app is listening on port ${config.port}`,
       )
     })
   } catch (err) {
-    console.log(`Failed to connect to db`, err)
+    errorLogger.error(`Failed to connect to db`, err)
   }
 }
 
-main().catch(err => console.log(err))
+main().catch(err => logger.error(err))
