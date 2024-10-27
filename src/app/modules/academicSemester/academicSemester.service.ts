0b7bca423b5f2ab57bd78b5456f9fb485bb90b4a
sowmik-sec <sowmik.sec@gmail.com>
2024-10-27 22:35:11 +0600

    Ceate Semester Code Validation in service layer

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,3 +1,5 @@
+import ApiError from '../../errors/ApiError'
+import { academicSemesterTitleCodeMapper } from './academicSemester.constant'
 import { IAcademicSemester } from './academicSemester.interface'
 import { AcademicSemester } from './academicSemester.model'
 
@@ -5,6 +7,9 @@ const createSemester = async (
   payload: IAcademicSemester,
 ): Promise<IAcademicSemester> => {
   const result = await AcademicSemester.create(payload)
+  if (academicSemesterTitleCodeMapper[payload.title] !== payload.code) {
+    throw new ApiError(400, 'Invalid semester code')
+  }
   return result
 }
 
