b38140b7947c1c8e06f88a237b42d78800238031
sowmik-sec <sowmik.sec@gmail.com>
2024-11-03 10:28:00 +0600

    Modify User Model , Schema _ Create Student Schema , Model

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,10 +1,13 @@
-import { Model } from 'mongoose'
+import { Model, Types } from 'mongoose'
 
 // THIS CODE SHOULD BE IN BRANCH SCRUM-5
 export type IUser = {
   id: string
   role: string
   password: string
+  student?: Types.ObjectId | IStudent
+  // faculty?: Types.ObjectId | IFaculty;
+  // admin?: Types.ObjectId | IAdmin;
 }
 
 export type UserModel = Model<IUser, Record<string, unknown>>
