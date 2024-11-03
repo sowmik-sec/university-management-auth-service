96f81b5e00e6f69b1ade83953a7a8c6615099876
sowmik-sec <sowmik.sec@gmail.com>
2024-11-03 10:51:37 +0600

    Face a bug , create mongoose schema and zod schema

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -2,10 +2,23 @@ import { z } from 'zod'
 
 const createUserZodSchema = z.object({
   body: z.object({
-    role: z.string({
-      required_error: 'role is required',
-    }),
     password: z.string().optional(),
+    student: z.object({
+      name: z.object({
+        firstName: z.string({ required_error: 'First Name is required' }),
+        middleName: z.string().optional(),
+        lastName: z
+          .string({ required_error: 'Last Name is required' })
+          .optional(),
+      }),
+      dateOfBirth: z.string({ required_error: 'Date of birth is required' }),
+      gender: z.enum(['male', 'female'], {
+        required_error: 'gender is required',
+      }),
+      bloodGroup: z.enum(['male', 'female'], {
+        required_error: 'blood group is required',
+      }),
+    }),
   }),
 })
 // await createUserZodSchema.parseAsync(req)
